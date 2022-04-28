7f2a71868dbf1449ca623435058959fe6de22726
TLCurd <TLCurdChiro@gmail.com>
2022-04-28 14:52:27 -0600

    add names_ids method to rec area model to use association for frontend

--- app/models/rec_area.rb
+++ app/models/rec_area.rb
@@ -2,15 +2,19 @@ class RecArea < ApplicationRecord
   has_many :rec_area_activities
   has_many :activities, :through => :rec_area_activities
 
-  def names_only
-    names = []
+  def names_ids
+    info = []
+    # ids = []
     i = 0 
     while i < activities.length
-      names << activities[i]["name"]
+      info << [[activities[i]["name"]], [activities[i]["id"]]]
+      # ids << activities[i]["id"]
       i += 1
     end
-    return names
+    return info
   end
+
+ 
 
 
 end
