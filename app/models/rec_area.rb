53976d5745d4b788cda6a5629501c7027edd4990
TLCurd <TLCurdChiro@gmail.com>
2022-04-28 00:35:25 -0600

    add names_only method (piggy-backing  activities associations) to rec_area model in order to only display only activity name

--- app/models/rec_area.rb
+++ app/models/rec_area.rb
@@ -1,4 +1,16 @@
 class RecArea < ApplicationRecord
   has_many :rec_area_activities
   has_many :activities, :through => :rec_area_activities
+
+  def names_only
+    names = []
+    i = 0 
+    while i < activities.length
+      names << activities[i]["name"]
+      i += 1
+    end
+    return names
+  end
+
+
 end
