b2e9c77efebf97a798dec8f0a68813601cbc69f5
TLCurd <TLCurdChiro@gmail.com>
2022-05-16 22:54:51 -0600

    add methods to rec_area model and rec_area show jb view for peaks within 10, 20, 30, 40, and 50 miles

--- app/models/rec_area.rb
+++ app/models/rec_area.rb
@@ -15,7 +15,109 @@ class RecArea < ApplicationRecord
     return info
   end
 
- 
+  def peaks_within_ten_miles
+    within_ten_miles = []
+    peaks = Peak.all
+    peaks.each do |peak|
+      distance = Geocoder::Calculations.distance_between([lat, long], [peak.lat, peak.long])
+      # p distance.class
+      if distance <= 10
+        within_ten_miles << {
+          peak_name: peak.name,
+          peak_id: peak.id,
+          distance: distance,
+          lat: peak.lat,
+          long: peak.long,
+        }
+      end
+    end
+    sorted = within_ten_miles.sort_by { |k| k[:distance]}
+    return sorted
+    # return within_ten_miles
+  end
 
+  def peaks_within_twenty_miles
+    within_twenty_miles = []
+    peaks = Peak.all
+    peaks.each do |peak|
+      distance = Geocoder::Calculations.distance_between([lat, long], [peak.lat, peak.long])
+      # p distance.class
+      if distance <= 20
+        within_twenty_miles << {
+          peak_name: peak.name,
+          peak_id: peak.id,
+          distance: distance,
+          lat: peak.lat,
+          long: peak.long,
+        }
+      end
+    end
+    sorted = within_twenty_miles.sort_by { |k| k[:distance]}
+    return sorted
+    # return within_twenty_miles
+  end
+
+  def peaks_within_thirty_miles
+    within_thirty_miles = []
+    peaks = Peak.all
+    peaks.each do |peak|
+      distance = Geocoder::Calculations.distance_between([lat, long], [peak.lat, peak.long])
+      # p distance.class
+      if distance <= 30
+        within_thirty_miles << {
+          peak_name: peak.name,
+          peak_id: peak.id,
+          distance: distance,
+          lat: peak.lat,
+          long: peak.long,
+        }
+      end
+    end
+    sorted = within_thirty_miles.sort_by { |k| k[:distance]}
+    return sorted
+    # return within_thirty_miles
+  end
+
+  def peaks_within_forty_miles
+    within_forty_miles = []
+    peaks = Peak.all
+    peaks.each do |peak|
+      distance = Geocoder::Calculations.distance_between([lat, long], [peak.lat, peak.long])
+      # p distance.class
+      if distance <= 40
+        within_forty_miles << {
+          peak_name: peak.name,
+          peak_id: peak.id,
+          distance: distance,
+          lat: peak.lat,
+          long: peak.long,
+        }
+      end
+    end
+    sorted = within_forty_miles.sort_by { |k| k[:distance]}
+    return sorted
+    # return within_forty_miles
+  end
+
+  def peaks_within_fifty_miles
+    within_fifty_miles = []
+    peaks = Peak.all
+    peaks.each do |peak|
+      distance = Geocoder::Calculations.distance_between([lat, long], [peak.lat, peak.long])
+      # p distance.class
+      if distance <= 50
+        within_fifty_miles << {
+          peak_name: peak.name,
+          peak_id: peak.id,
+          distance: distance,
+          lat: peak.lat,
+          long: peak.long,
+        }
+      end
+    end
+    sorted = within_fifty_miles.sort_by { |k| k[:distance]}
+    return sorted
+    # return within_fifty_miles
+  end
 
 end
